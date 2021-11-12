65387b1b1ed113011c7dd287562b47668a3025b3
moldir <moldirshb@gmail.com>
2021-11-12 15:06:25 -0500

    deployment

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://little-explorer-api.herokuapp.com/'
+    ? 'https://little-explorers.herokuapp.com/'
     : 'http://localhost:3000'
 
 const api = axios.create({
